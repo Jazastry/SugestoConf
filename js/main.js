46754c8327398c63a656f0161c24a2a43b6b1d26
Jazastry <yogib@mail.bg>
2015-06-11 10:48:37 +0300

    Buttons structured, JS optimization, logo added, css optimized.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,39 @@
-// SMOOTH SCROLL
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+// APP - MAIN FUNCTION CONTAINER
+var app = (function(){  
+  function smoothScroll(target) {
+    if (location.pathname.replace(/^\//,'') == target.pathname.replace(/^\//,'') &&
+       location.hostname == target.hostname) {
 
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      var target = $(target.hash);
+      target = target.length ? target : $('[name=' + target.hash.slice(1) +']');
       if (target.length) {
         $('html,body').animate({
           scrollTop: target.offset().top
         }, 1000);
         return false;
-      }
+     }
     }
-  });
-});
+  }
 
-// CHANGE COLOR NAV BTN SELECTED
-$('#navbar > ul > li').click(function (e) {
-	console.log('in change');
+  function manageActiveClass(e) {
     e.preventDefault();
-    $('ul.nav > li').removeClass('active');
-    $(this).addClass('active');
+    $('#navbar > ul > li').removeClass('active');
+    $(e.currentTarget).parent().addClass('active');
+  }
+
+  return {
+    smoothScroll: smoothScroll,
+    manageActiveClass: manageActiveClass
+  };
+
+}());
+
+
+
+// SMOOTH SCROLL & CHANGE ACTIVE BTN
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function(e) {
+    app.manageActiveClass(e);
+    app.smoothScroll(e.currentTarget);  
+  });
 });
